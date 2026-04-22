50ea9d7b5435ce3825b5e88d21e6ad90ff666dc2
binhgold689 <binhdubai.68@gmail.com>
2026-04-22 15:09:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
-import requests
+import random
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "running"}
+    return {"status":"running"}
 
-@app.get("/price/{symbol}")
-def price(symbol: str):
-    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol.upper()}"
-    return requests.get(url).json()
+@app.get("/signals")
+def signals():
+    return [
+        {
+            "pair":"BTCUSDT",
+            "type":"LONG",
+            "entry":68400,
+            "sl":67800,
+            "tp":69500,
+            "confidence":87
+        },
+        {
+            "pair":"ETHUSDT",
+            "type":"SHORT",
+            "entry":3480,
+            "sl":3560,
+            "tp":3320,
+            "confidence":82
+        }
+    ]
